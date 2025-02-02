80570a98e9a4ac0433efdb81f0f3cbdfe261c68a
flaim <105062508+flaim4@users.noreply.github.com>
2025-02-03 01:43:07 +0300

    render image

--- src/main/java/net/flaim/guiding_skint/client/HUDHandler.java
+++ src/main/java/net/flaim/guiding_skint/client/HUDHandler.java
@@ -2,6 +2,7 @@ package net.flaim.guiding_skint.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.flaim.guiding_skint.GuidingSkintMod;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -13,7 +14,9 @@ public class HUDHandler implements ResourceManagerReloadListener, IGuiOverlay {
 
     public static final HUDHandler INSTANCE = new HUDHandler();
 
-    public static final ResourceLocation GUI_ICONS_LOCATION = new ResourceLocation("textures/gui/guiding_skint_cleared");
+    public static final ResourceLocation GUI_ICONS_LOCATION = new ResourceLocation(GuidingSkintMod.MOD_ID, "textures/gui/guiding_skint_cleared.png");
+
+
 
     @Override
     public void onResourceManagerReload(ResourceManager resourceManager) {
@@ -21,7 +24,20 @@ public class HUDHandler implements ResourceManagerReloadListener, IGuiOverlay {
     }
 
     @Override
-    public void render(ForgeGui forgeGui, GuiGraphics guiGraphics, float v, int i, int i1) {
-        guiGraphics.blit(GUI_ICONS_LOCATION, 100, 100, 0, 139, 100, 28);
+    public void render(ForgeGui forgeGui, GuiGraphics guiGraphics, float partialTicks, int mouseX, int mouseY) {
+        PoseStack poseStack = guiGraphics.pose();
+        poseStack.pushPose();
+
+        int baseWidth = 418;
+        int baseHeight = 25;
+
+        RenderSystem.setShaderTexture(0, GUI_ICONS_LOCATION);
+
+        guiGraphics.blit(GUI_ICONS_LOCATION, 10, 10, 0, 0, baseWidth, baseHeight, baseWidth, baseHeight);
+
+        poseStack.popPose();
     }
+
+
+
 }
