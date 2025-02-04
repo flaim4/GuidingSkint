fd4f13f9619c8cb2bce7c6940abd80131ee50ac1
flaim <105062508+flaim4@users.noreply.github.com>
2025-02-04 03:41:54 +0300

    >:3

--- src/main/java/net/flaim/guiding_skint/client/HUDHandler.java
+++ src/main/java/net/flaim/guiding_skint/client/HUDHandler.java
@@ -15,7 +15,7 @@ public class HUDHandler implements ResourceManagerReloadListener, IGuiOverlay {
     public static final HUDHandler INSTANCE = new HUDHandler();
 
     public static final ResourceLocation SKINT_CLEAR = new ResourceLocation(GuidingSkintMod.MOD_ID, "textures/gui/guiding_skint_cleared.png");
-    public static final ResourceLocation SKINT_CLEAR_LINE = new ResourceLocation(GuidingSkintMod.MOD_ID, "textures/gui/line.png");
+    public static final ResourceLocation SKINT_CLEAR_LINE = new ResourceLocation(GuidingSkintMod.MOD_ID, "textures/gui/skint_line.png");
     public static final ResourceLocation SKINT_CLEAR_SHADOW = new ResourceLocation(GuidingSkintMod.MOD_ID, "textures/gui/guiding_skint_cleared_shadow.png");
 
 
@@ -40,12 +40,15 @@ public class HUDHandler implements ResourceManagerReloadListener, IGuiOverlay {
         if (currentTime < displayEndTime) {
             float alpha = 1.0f;
             long elapsed = currentTime - showStartTime;
+
             if (elapsed < fadeDuration) {
                 alpha = elapsed / (float) fadeDuration;
-            }
-            else if (currentTime > displayEndTime - fadeDuration) {
+            } else if (currentTime > displayEndTime - fadeDuration) {
                 alpha = (displayEndTime - currentTime) / (float) fadeDuration;
             }
+
+            float progress = elapsed / (float) totalDisplayTime;
+            float scale = 1.0f + 0.05f * progress;
 
             PoseStack poseStack = guiGraphics.pose();
             poseStack.pushPose();
@@ -53,16 +56,31 @@ public class HUDHandler implements ResourceManagerReloadListener, IGuiOverlay {
             RenderSystem.enableBlend();
             RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, alpha);
 
-
             int screenWidth = forgeGui.getMinecraft().getWindow().getGuiScaledWidth();
             int screenHeight = forgeGui.getMinecraft().getWindow().getGuiScaledHeight();
 
+            int shadowWidth = 506, shadowHeight = 112;
+            int lineWidth = 437, lineHeight = 26;
+            int baseWidth = 418, baseHeight = 25;
+
+            int shadowX = (int) ((screenWidth - shadowWidth * scale) / 2);
+            int shadowY = (int) ((screenHeight - shadowHeight * scale) / 2);
+            int baseX = (int) ((screenWidth - baseWidth * scale) / 2);
+            int baseY = (int) ((screenHeight - baseHeight * scale) / 2);
+
+            poseStack.scale(scale, scale, 1.0f);
+
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, alpha);
             RenderSystem.setShaderTexture(0, SKINT_CLEAR_SHADOW);
-            guiGraphics.blit(SKINT_CLEAR_SHADOW, (screenWidth - 506)/ 2, (screenHeight - 112) / 2, 0, 0, 506, 112, 506, 112);
+            guiGraphics.blit(SKINT_CLEAR_SHADOW, shadowX, shadowY, 0, 0, shadowWidth, shadowHeight, shadowWidth, shadowHeight);
 
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, alpha);
+            RenderSystem.setShaderTexture(0, SKINT_CLEAR_LINE);
+            guiGraphics.blit(SKINT_CLEAR_LINE, (int) ((screenWidth - lineWidth * scale) / 2), (int) ((screenHeight - lineHeight * scale) / 2), 0, 0, lineWidth, lineHeight, lineWidth, lineHeight);
+
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, alpha);
             RenderSystem.setShaderTexture(0, SKINT_CLEAR);
-            guiGraphics.blit(SKINT_CLEAR, (screenWidth - 418) / 2, (screenHeight - 25) / 2, 0, 0, 418, 25, 418, 25);
-
+            guiGraphics.blit(SKINT_CLEAR, baseX, baseY, 0, 0, baseWidth, baseHeight, baseWidth, baseHeight);
 
             RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
             RenderSystem.disableBlend();
@@ -70,6 +88,10 @@ public class HUDHandler implements ResourceManagerReloadListener, IGuiOverlay {
             poseStack.popPose();
         }
     }
+
+
+
+
 
 
 
