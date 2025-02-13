f90c209853596c01d1f29f4d79d9c03473a630cd
flaim <likeyt60@gmail.com>
2025-02-14 00:50:35 +0300

    ;-;

--- src/main/java/net/flaim/guiding_skint/client/RenderUI.java
+++ src/main/java/net/flaim/guiding_skint/client/RenderUI.java
@@ -14,23 +14,20 @@ public class RenderUI {
 
     static Minecraft minecraft = Minecraft.getInstance();
 
-    static int windowWidth;
-    static int windowHeight;
-    static Scroll scroll = new Scroll(minecraft, 10, 300, 0, 100);
+    static Scroll scroll;
 
     public static void render(GuiGraphics guiGraphics) {
-        windowWidth = minecraft.getWindow().getGuiScaledWidth();
-        windowHeight = minecraft.getWindow().getGuiScaledHeight();
+        int windowWidth = minecraft.getWindow().getGuiScaledWidth();
+        int windowHeight = minecraft.getWindow().getGuiScaledHeight();
 
-//        guiGraphics.fill(
-//                (windowWidth - 300) / 2,
-//                (windowHeight - 300) / 2,
-//                (windowWidth - 300) / 2 + 300,
-//                (windowHeight - 300) / 2 + 300,
-//                0,
-//                color.hashCode()
-//        );
-        scroll.render(guiGraphics, 0, 50, 20);
+        int scrollWidth = 300;
+        int scrollHeight = 300;
+        int scrollX = (windowWidth - scrollWidth) / 2;
+        int scrollY = (windowHeight - scrollHeight) / 2;
+
+        scroll = new Scroll(minecraft, scrollWidth, scrollHeight, scrollY, scrollX);
+
+        scroll.render(guiGraphics, 0, 0, 0);
     }
 
 }
--- src/main/java/net/flaim/guiding_skint/client/widget/Scroll.java
+++ src/main/java/net/flaim/guiding_skint/client/widget/Scroll.java
@@ -1,5 +1,6 @@
 package net.flaim.guiding_skint.client.widget;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.Tesselator;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -7,15 +8,19 @@ import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraftforge.client.gui.widget.ScrollPanel;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Scroll extends ScrollPanel {
-
     static Minecraft minecraft = Minecraft.getInstance();
 
-    static int windowWidth;
-    static int windowHeight;
+    ArrayList<String> formattedCharSequence = new ArrayList<>(Arrays.asList(
+            "Текст 1", "Текст 2", "Текст 3", "Текст 4", "Текст 5", "Текст 6", "Текст 7", "Текст 8"
+    ));
 
     static Color color = new Color(0.0f, 0.0f, 0.0f, 0.6f);
+
+    private double scrollDistance = 0;
 
     public Scroll(Minecraft client, int width, int height, int top, int left) {
         super(client, width, height, top, left);
@@ -23,19 +28,49 @@ public class Scroll extends ScrollPanel {
 
     @Override
     protected int getContentHeight() {
-        return 0;
+        return formattedCharSequence.size() * 50;
     }
 
     @Override
     protected void drawPanel(GuiGraphics guiGraphics, int i, int i1, Tesselator tesselator, int i2, int i3) {
-        guiGraphics.fill(
-                (windowWidth - 300) / 2,
-                (windowHeight - 300) / 2,
-                (windowWidth - 300) / 2 + 300,
-                (windowHeight - 300) / 2 + 300,
-                0,
-                color.hashCode()
-        );
+        int yPos = i1 - (int) scrollDistance;
+
+        for (String line : formattedCharSequence) {
+            if (line != null) {
+                RenderSystem.enableBlend();
+                guiGraphics.drawString(minecraft.font, line, this.left + 6, yPos, 0xFFFFFF);
+                RenderSystem.disableBlend();
+            }
+            yPos += 50;
+        }
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double scroll) {
+        if (scroll != 0) {
+            scrollDistance += (scroll < 0 ? 20 : -20);
+            applyScrollLimits();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    private void applyScrollLimits() {
+        int maxScroll = getMaxScroll();
+        if (scrollDistance < 0) {
+            scrollDistance = 0;
+        } else if (scrollDistance > maxScroll) {
+            scrollDistance = maxScroll;
+        }
+    }
+
+    private int getMaxScroll() {
+        return getContentHeight() - (this.height - this.border);
     }
 
     @Override
@@ -45,6 +80,5 @@ public class Scroll extends ScrollPanel {
 
     @Override
     public void updateNarration(NarrationElementOutput narrationElementOutput) {
-
     }
 }
